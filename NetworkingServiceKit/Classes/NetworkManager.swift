582ff9274e93108c1d42d906b6928639f672a18f
Philli.pe Casorla Sagot <phillipe@makespace.com>
2017-03-08 14:23:14 +0100

    Move HTTPMethod to Int32

--- NetworkingServiceKit/Classes/NetworkManager.swift
+++ NetworkingServiceKit/Classes/NetworkManager.swift
@@ -8,28 +8,46 @@
 
 import Foundation
 
-public enum HTTPMethod: String {
-    case options = "OPTIONS"
-    case get     = "GET"
-    case head    = "HEAD"
-    case post    = "POST"
-    case put     = "PUT"
-    case patch   = "PATCH"
-    case delete  = "DELETE"
-    case trace   = "TRACE"
-    case connect = "CONNECT"
+@objc
+public enum HTTPMethod: Int32 {
+    case options
+    case get
+    case head
+    case post
+    case put
+    case patch
+    case delete
+    case trace
+    case connect
+    
+    var string:String {
+        switch self {
+        case .options: return "OPTIONS"
+        case .get: return "GET"
+        case .head: return "HEAD"
+        case .post: return "POST"
+        case .put: return "PUT"
+        case .patch: return "PATCH"
+        case .delete: return "DELETE"
+        case .trace: return "TRACE"
+        case .connect: return "CONNECT"
+        }
+        return ""
+    }
 }
 public typealias SuccessResponseBlock = ([String:Any]) -> Void
-public typealias ErrorBlock = (Error) -> Void
+public typealias ErrorResponseBlock = (Error,[String:Any]?) -> Void
 
+@objc
 public protocol NetworkManager
 {
     var configuration:APIConfiguration {get set}
     func request(path: String,
                  method: HTTPMethod,
                  with parameters: [String: Any],
+                 paginated:Bool,
                  success: @escaping SuccessResponseBlock,
-                 failure: @escaping ErrorBlock)
+                 failure: @escaping ErrorResponseBlock)
     
     init(with configuration:APIConfiguration)
 }
