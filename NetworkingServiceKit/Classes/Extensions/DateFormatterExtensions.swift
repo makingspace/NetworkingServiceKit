501bcccd4c5d481a35927387d22e3b0e17a9e3ec
Philli.pe Casorla Sagot <phillipe@makespace.com>
2017-03-15 18:21:06 +0100

    Make date exstensions public again

--- NetworkingServiceKit/Classes/Extensions/DateFormatterExtensions.swift
+++ NetworkingServiceKit/Classes/Extensions/DateFormatterExtensions.swift
@@ -10,36 +10,36 @@ import Foundation
 
 extension DateFormatter {
     
-    class internal func makespace() -> DateFormatter {
+    class public func makespace() -> DateFormatter {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
         dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
         return dateFormatter
     }
     
-    class internal func makespaceDateFormatterWithMicroSeconds() -> DateFormatter {
+    class public func makespaceDateFormatterWithMicroSeconds() -> DateFormatter {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ"
         return dateFormatter
     }
     
-    class internal func makespaceHourMinute() -> DateFormatter {
+    class public func makespaceHourMinute() -> DateFormatter {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "h:mm a"
         return dateFormatter
     }
     
-    class internal func makespaceYearMonthDay() -> DateFormatter {
+    class public func makespaceYearMonthDay() -> DateFormatter {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
         return dateFormatter
     }
     
-    class internal func string(fromMSDate msDate: Date) -> String {
+    class public func string(fromMSDate msDate: Date) -> String {
         return self.makespace().string(from: msDate)
     }
     
-    class internal func date(fromMSString dateString: String) -> Date? {
+    class public func date(fromMSString dateString: String) -> Date? {
         return self.makespace().date(from: dateString)
     }
 }
