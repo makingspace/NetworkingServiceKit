c3151bd14fcd622d130ec050be96a2d7c854d2e6
Philli.pe Casorla Sagot <phillipe@makespace.com>
2017-05-08 10:39:11 +0200

    Implement twitter example with own protocols impls

---
+++ Example/NetworkingServiceKit/TwitterSearchService.swift
@@ -0,0 +1,104 @@
+//
+//  TwitterSearchService.swift
+//  Makespace Inc.
+//
+//  Created by Phillipe Casorla Sagot (@darkzlave) on 4/24/17.
+//
+//
+
+import Foundation
+import NetworkingServiceKit
+
+public struct TwitterUser
+{
+    public let handle:String
+    public let imagePath:String
+    public let location: String
+}
+public struct TwitterSearchResult
+{
+    public let tweet: String
+    public let user: TwitterUser
+}
+open class TwitterSearchService: AbstractBaseService
+{
+    private var nextResultsPage:String?
+    open override var serviceVersion: String
+    {
+        return "1.1"
+    }
+    
+    public func searchRecents(by hashtag:String,
+                              completion:@escaping (_ results:[TwitterSearchResult])-> Void) {
+        guard !hashtag.isEmpty else {
+            completion([TwitterSearchResult]())
+            return
+        }
+        let parameters = ["q" : hashtag]
+        request(path: "search/tweets.json",
+                method: .get,
+                with: parameters,
+                success: { response in
+                    var searchResults = [TwitterSearchResult]()
+                    if let results = response["statuses"] as? [[String:Any]] {
+                        for result in results {
+                            if let tweet = result["text"] as? String,
+                                let user = result["user"] as? [String:Any],
+                                let userHandle = user["screen_name"] as? String,
+                                let userImagePath = user["profile_image_url_https"] as? String,
+                                let location = user["location"] as? String
+                            {
+                                searchResults.append(TwitterSearchResult(tweet: tweet,
+                                                                         user: TwitterUser(handle: userHandle,
+                                                                                           imagePath:userImagePath,
+                                                                                           location: location)))
+                            }
+                        }
+                    }
+                    //save next page
+                    if let metadata = response["search_metadata"] as? [String:Any],
+                        let nextPage = metadata["next_results"] as? String{
+                        self.nextResultsPage = nextPage
+                    }
+                    completion(searchResults)
+        }, failure: { error, errorResponse in
+            completion([TwitterSearchResult]())
+        })
+    }
+    
+    public func searchNextRecentsPage(completion:@escaping (_ results:[TwitterSearchResult])-> Void) {
+        guard let nextPage = self.nextResultsPage else {
+            completion([TwitterSearchResult]())
+            return
+        }
+        request(path: "search/tweets.json\(nextPage)",
+                method: .get,
+                with: [:],
+                success: { response in
+                    var searchResults = [TwitterSearchResult]()
+                    if let results = response["statuses"] as? [[String:Any]] {
+                        for result in results {
+                            if let tweet = result["text"] as? String,
+                                let user = result["user"] as? [String:Any],
+                                let userHandle = user["screen_name"] as? String,
+                                let userImagePath = user["profile_image_url_https"] as? String,
+                                let location = user["location"] as? String
+                            {
+                                searchResults.append(TwitterSearchResult(tweet: tweet,
+                                                                         user: TwitterUser(handle: userHandle,
+                                                                                           imagePath:userImagePath,
+                                                                                           location: location)))
+                            }
+                        }
+                    }
+                    //save next page
+                    if let metadata = response["search_metadata"] as? [String:Any],
+                        let nextPage = metadata["next_results"] as? String{
+                        self.nextResultsPage = nextPage
+                    }
+                    completion(searchResults)
+        }, failure: { error, errorResponse in
+            completion([TwitterSearchResult]())
+        })
+    }
+}
