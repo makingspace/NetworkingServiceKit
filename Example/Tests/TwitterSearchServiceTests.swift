0a7acc22f29b8d49e0128119b79f9ea9942423e3
Philli.pe Casorla Sagot <phillipe@makespace.com>
2017-06-02 17:26:19 +0200

    Fix search tests

--- Example/Tests/TwitterSearchServiceTests.swift
+++ Example/Tests/TwitterSearchServiceTests.swift
@@ -11,11 +11,29 @@ import Nimble
 import NetworkingServiceKit
 import Mockingjay
 
-class TwitterSearchServiceTests: QuickSpec {
+class TwitterSearchServiceTestsStubbed: QuickSpec {
     
     override func spec() {
+        
+        let searchStub = ServiceStub(execute: ServiceStubRequest(path: "/1.1/search/tweets.json", parameters: ["q" : "#makespace"]),
+                                     with: .success(code: 200, response: ["statuses" : [["text" : "tweet1" ,
+                                                                                         "user" : ["screen_name" : "darkzlave",
+                                                                                                   "profile_image_url_https" : "https://lol.png",
+                                                                                                   "location" : "Stockholm, Sweden"]],
+                                                                                        ["text" : "tweet2" ,
+                                                                                         "user" : ["screen_name" : "makespace",
+                                                                                                   "profile_image_url_https" : "https://lol2.png",
+                                                                                                   "location" : "New York"]]],
+                                                                          "search_metadata" : ["next_results" : "https://search.com/next?pageId=2"]
+                                        ]))
+        let searchEmptyStub = ServiceStub(execute: ServiceStubRequest(path: "/1.1/search/tweets.json", parameters: ["q" : ""]),
+                                          with: .success(code: 200, response: [:]))
+        let searchFail = ServiceStub(execute: ServiceStubRequest(path: "/1.1/search/tweets.json", parameters: ["q" : "random"]),
+                                                    with: .failure(code:500, response:[:]))
+        let searchService = ServiceLocator.service(forType: TwitterSearchService.self, stubs: [searchStub,searchEmptyStub,searchFail])
+        
         beforeEach {
-            ServiceLocator.defaultNetworkClientType = AlamoNetworkManager.self
+            ServiceLocator.defaultNetworkClientType = StubNetworkManager.self
             ServiceLocator.reset()
             ServiceLocator.set(services: [TwitterSearchService.self],
                                api: TwitterAPIConfigurationType.self,
@@ -25,36 +43,20 @@ class TwitterSearchServiceTests: QuickSpec {
         describe("when doing a search request") {
             context("with a proper query") {
                 it("should correctly parse and return the search results as objects") {
-                    let searchResponse = ["statuses" : [["text" : "tweet1" ,
-                                                         "user" : ["screen_name" : "darkzlave",
-                                                                   "profile_image_url_https" : "https://lol.png",
-                                                                   "location" : "Stockholm, Sweden"]],
-                                                        ["text" : "tweet2" ,
-                                                         "user" : ["screen_name" : "makespace",
-                                                                   "profile_image_url_https" : "https://lol2.png",
-                                                                   "location" : "New York"]]],
-                                          "search_metadata" : ["next_results" : "https://search.com/next?pageId=2"]
-                        ] as [String : Any]
-                    MockingjayProtocol.addStub(matcher: http(.get, uri: "/1.1/search/tweets.json"), builder: json(searchResponse))
                     
-                    waitUntil { done in
-                        let searchService = ServiceLocator.service(forType: TwitterSearchService.self)
-                        searchService?.searchRecents(by: "#makespace", completion: { results in
-                            expect(results.count).to(equal(2))
-                            let resultFirst = results.first
-                            expect(resultFirst).toNot(beNil())
-                            expect(resultFirst?.tweet).to(equal("tweet1"))
-                            expect(resultFirst?.user.handle).to(equal("darkzlave"))
-                            expect(resultFirst?.user.imagePath).to(equal("https://lol.png"))
-                            done()
-                        })
-                    }
+                    searchService?.searchRecents(by: "#makespace", completion: { results in
+                        expect(results.count).to(equal(2))
+                        let resultFirst = results.first
+                        expect(resultFirst).toNot(beNil())
+                        expect(resultFirst?.tweet).to(equal("tweet1"))
+                        expect(resultFirst?.user.handle).to(equal("darkzlave"))
+                        expect(resultFirst?.user.imagePath).to(equal("https://lol.png"))
+                    })
                 }
             }
             
             context("with an empty query") {
                 it("should return immediatly with no results") {
-                    let searchService = ServiceLocator.service(forType: TwitterSearchService.self)
                     searchService?.searchRecents(by: "", completion: { results in
                         expect(results.count).to(equal(0))
                     })
@@ -65,17 +67,25 @@ class TwitterSearchServiceTests: QuickSpec {
         describe("when doing a failed search request") {
             context("with a proper query") {
                 it("should return no results") {
-                    MockingjayProtocol.addStub(matcher: http(.get, uri: "/1.1/search/tweets.json"), builder: http(500))
-                    
-                    waitUntil { done in
-                        let searchService = ServiceLocator.service(forType: TwitterSearchService.self)
-                        searchService?.searchRecents(by: "#makespace", completion: { results in
-                            expect(results.count).to(equal(0))
-                            done()
-                        })
-                    }
+                    searchService?.searchRecents(by: "#makespace", completion: { results in
+                        expect(results.count).to(equal(0))
+                    })
                 }
             }
+        }
+    }
+}
+
+class TwitterSearchServiceTestsAlamo: QuickSpec {
+    override func spec() {
+        
+        beforeEach {
+            ServiceLocator.defaultNetworkClientType = AlamoNetworkManager.self
+            ServiceLocator.reset()
+            ServiceLocator.set(services: [TwitterSearchService.self],
+                               api: TwitterAPIConfigurationType.self,
+                               auth: TwitterApp.self,
+                               token: TwitterAPIToken.self)
         }
         
         describe("when doing a search request") {
@@ -93,7 +103,7 @@ class TwitterSearchServiceTests: QuickSpec {
                         ] as [String : Any]
                     MockingjayProtocol.addStub(matcher: http(.get, uri: "/1.1/search/tweets.json"), builder: json(searchResponse))
                     MockingjayProtocol.addStub(matcher: http(.get, uri: "/1.1/search/tweets.json?id=2"), builder: json(searchResponse))
-                    
+
                     waitUntil { done in
                         let searchService = ServiceLocator.service(forType: TwitterSearchService.self)
                         searchService?.searchRecents(by: "#makespace", completion: { results in
